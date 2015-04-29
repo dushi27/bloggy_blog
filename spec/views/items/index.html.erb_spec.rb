2939b142eec264a8849d95fac5815cfa86b34055
dushi27 <dushi27@gmail.com>
2015-04-29 23:16:03 +0000

    first commit

---
+++ spec/views/items/index.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "items/index", type: :view do
+  before(:each) do
+    assign(:items, [
+      Item.create!(
+        :name => "Name",
+        :price => "Price"
+      ),
+      Item.create!(
+        :name => "Name",
+        :price => "Price"
+      )
+    ])
+  end
+
+  it "renders a list of items" do
+    render
+    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => "Price".to_s, :count => 2
+  end
+end
